8eb3a26bb12b9e4bfca531e390c759208a630d56
Mark Abuyabor <mark@zeguestlist.com>
2022-10-02 16:40:15 +0800

    finishing up

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,20 +1,78 @@
-import { useState, useTransition } from 'react';
+import { useState } from 'react';
 
 const Group = ({employees, selectedTeam, setTeam}) => {
 
-    const [groupEmployees, setGroupEmployees] = useState(groupTeamEmployees());
-
-    const groupTeamEmployees = () => {
+    
+    const groupTeamMembers = () => {
         let teams = [];
+
+        let teamAMembers = employees.filter((employee) => employee.teamName === "TeamA");
+        let teamA = { id: 1, team: 'TeamA', members: teamAMembers, collapsed: selectedTeam === 'TeamA' ? false : true }
+        teams.push(teamA);
+
+        let teamBMembers = employees.filter((employee) => employee.teamName === "TeamB");
+        let teamB = { id: 2, team: 'TeamB', members: teamBMembers, collapsed: selectedTeam === 'TeamB' ? false : true }
+        teams.push(teamB);
+
+        let teamCMembers = employees.filter((employee) => employee.teamName === "TeamC");
+        let teamC = { id: 3, team: 'TeamC', members: teamCMembers, collapsed: selectedTeam === 'TeamC' ? false : true }
+        teams.push(teamC);
+
+        let teamDMembers = employees.filter((employee) => employee.teamName === "TeamD");
+        let teamD = {id: 4, team: 'TeamD', members: teamDMembers, collapsed: selectedTeam === 'TeamD' ? false : true }
+        teams.push(teamD);
+           
+        return teams;
+    };
+
+
+    const [groupEmployees, setGroupEmployees] = useState(groupTeamMembers());
+
+
+
+    const handleTeamClick = (event) => {
+        let newGroupData = groupEmployees.map((groupedData) => {
+            return groupedData.team === event.currentTarget.id ? {...groupedData, collapsed: !groupedData.collapsed} : groupedData
+        });
+
+        setGroupEmployees(newGroupData);
+        setTeam(event.currentTarget.id);
     };
 
     return ( 
         <main className="container">
-            <div className="row justify-content-center mt-3 mb-4">
-                <div className="col-8">
-                    <h1>Group Team</h1>
-                </div>
-            </div>
+           {
+            groupEmployees.map((item,idx) => {
+                return (
+                    <div key={item.id} className="card mt-2" style={{cursor: "pointer"}}>
+                        <h4 id={item.team} 
+                        className="card-header text-secondary bg-white"
+                        onClick={handleTeamClick}>
+                            Team Name: {item.team}
+                        </h4>
+
+                        <div id={"collapse" + item.team}
+                            className={item.collapsed === true? "collapse": ""}
+                        >
+                        <hr />
+                        {
+                            item.members.map((member) => {
+                                return (
+                                    <div className="mt-2">
+                                        <h5 className="card-title">
+                                            <span className="text-dark">Full Name: {member.fullName}</span>
+                                        </h5>
+                                        <p>Designation: { member.designation }</p>
+                                    </div>
+                                )
+                            })
+                        }
+
+                        </div>
+                    </div>
+                )
+            })
+           }
         </main>
      );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return ( 
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <ul className="navbar-nav mx-auto mb-2 mb-lg-8">
-                <li class="nav-item">
+                <li className="nav-item">
                     <Link className="nav-link" to="/">Home</Link>
                 </li>
-                <li class="nav-item">
+                <li className="nav-item">
                     <Link className="nav-link" to="/group">Group</Link>
                 </li>
             </ul>
